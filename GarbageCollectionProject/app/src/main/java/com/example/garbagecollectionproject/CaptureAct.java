a41d9743e07f4c56961c61bab89f76171790faef
Abhishek Acharya <93998616+Abhishek-Acharya1@users.noreply.github.com>
2023-05-14 23:14:53 +0530

    Delete GarbageCollectionProject directory

--- GarbageCollectionProject/app/src/main/java/com/example/garbagecollectionproject/CaptureAct.java
+++
@@ -1,6 +0,0 @@
-package com.example.garbagecollectionproject;
-
-import com.journeyapps.barcodescanner.CaptureActivity;
-
-public class CaptureAct  extends CaptureActivity {
-}
--- GarbageCollectionProject/app/src/main/java/com/example/garbagecollectionproject/scanner_qr_user.java
+++
@@ -1,9 +0,0 @@
-package com.example.garbagecollectionproject;
-
-public class scanner_qr_user {
-    public int val;
-    public scanner_qr_user(){};
-    public scanner_qr_user(int val){
-        this.val=val;
-    }
-}
